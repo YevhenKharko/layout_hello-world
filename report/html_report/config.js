228f141e7c9a25b20ccc0a5eb2c41db73125a21f
YevhenKharko <yevhenkharko@gmail.com>
2023-07-28 12:32:59 +0300

    make build

--- report/html_report/config.js
+++ report/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "..\\bitmaps_reference\\Entire_document.png",
-        "test": "..\\bitmaps_test\\20230728-122432\\Entire_document.png",
+        "test": "..\\bitmaps_test\\20230728-123009\\Entire_document.png",
         "selector": "document",
         "fileName": "Entire_document.png",
         "label": "Entire document",
